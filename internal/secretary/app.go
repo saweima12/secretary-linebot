aeaab92bcc5dc6e69429480d2e9f91688ec06f2b
saweima12 <saweima12@gmail.com>
2023-03-14 01:14:12 +0800

    Add mongodb & linebot instance to app struct.

--- internal/secretary/app.go
+++ internal/secretary/app.go
@@ -1,18 +1,44 @@
 package secretary
 
 import (
+	"context"
+	"log"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/line/line-bot-sdk-go/linebot"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type ServerApp struct {
 	Engine *gin.Engine
 	Bot    *linebot.Client
+	Mongo  *mongo.Client
 }
 
 func (app *ServerApp) Run(port string) error {
-	err := app.Engine.Run(port)
+	// Create database connection.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := app.Mongo.Connect(ctx)
 	if err != nil {
 		return err
 	}
+
+	// check db connection is ok.
+	err = app.Mongo.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+		return err
+	}
+
+	// Start gin server.
+	err = app.Engine.Run(port)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,38 +1,74 @@
 package server
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/line/line-bot-sdk-go/linebot"
-	modules "github.com/saweima12/secretary-bot/internal/secretary"
+	"github.com/saweima12/secretary-bot/internal/handler"
+	"github.com/saweima12/secretary-bot/internal/secretary"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
-	BOT_SECRET = "BOT_LINE_SECRET"
-	BOT_TOKEN  = "BOT_LINE_TOKEN"
+	ENV_BOT_SECRET = "BOT_LINE_SECRET"
+	ENV_BOT_TOKEN  = "BOT_LINE_TOKEN"
+	ENV_MONGO_URI  = "BOT_MONGO_URI"
 )
 
-func New() *modules.ServerApp {
-	// get bot channel secret & token from environment.
-	secret := os.Getenv(BOT_SECRET)
-	token := os.Getenv(BOT_TOKEN)
+func New() *secretary.ServerApp {
+	// get bot channel botSecret & token from environment.
+	botSecret := os.Getenv(ENV_BOT_SECRET)
+	botToken := os.Getenv(ENV_BOT_TOKEN)
+
+	if botSecret == "" || botToken == "" {
+		panic("Please set environment variable BOT_LINE_SECRET and BOT_LINE_TOKEN")
+	}
 
 	// declare gin server.
 	engine := gin.Default()
-	bot, err := linebot.New(secret, token)
 
+	// intialize Linebot client.
+	bot, err := linebot.New(botSecret, botToken)
 	if err != nil {
 		panic(err)
 	}
 
-	// engine.SetTrustedProxies([]string{"172.17.0.1"})
+	// get mongodb uri from environment variable.
+	dbUri := os.Getenv(ENV_MONGO_URI)
 
-	// create app instance.
-	app := &modules.ServerApp{
-		Engine: engine,
-		Bot:    bot,
+	// create mongodb Client.
+	dbClient, err := initMongoDB(dbUri)
+	if err != nil {
+		panic(err)
 	}
 
+	// create app instance.
+	app := &secretary.ServerApp{
+		Engine: engine,
+		Bot:    bot,
+		Mongo:  dbClient,
+	}
+
+	// register router.
+	router := handler.NewRouter(app)
+	router.Init()
+
 	return app
+}
+
+func initMongoDB(dbUri string) (*mongo.Client, error) {
+	// declare options & ctx
+
+	log.Printf("TEST DBURI: %s", dbUri)
+	opts := options.Client().ApplyURI(dbUri)
+	client, err := mongo.NewClient(opts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
