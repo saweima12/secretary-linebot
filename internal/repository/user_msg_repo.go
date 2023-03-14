95b66436ce016328e2ca5213cd3c3c4adc063ab0
saweima12 <saweima12@gmail.com>
2023-03-15 00:49:40 +0800

    Implement repository logic - Add FetchOne function to get message by userId - Add InsertOne function to insert message data to database.

--- internal/repository/user_msg_repo.go
+++ internal/repository/user_msg_repo.go
@@ -1,10 +1,18 @@
 package repository
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"time"
+
+	"github.com/saweima12/secretary-bot/internal/entity"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type UserMsgRepository interface {
-	InsertOne()
-	FetchOne()
+	InsertOne(msg entity.UserMsg) (*mongo.InsertOneResult, error)
+	FetchOne(userId string) *mongo.SingleResult
 }
 
 type userMsgRepository struct {
@@ -12,12 +20,30 @@ type userMsgRepository struct {
 }
 
 func NewBotMessageRepository(collection *mongo.Collection) UserMsgRepository {
+	opts := options.CreateIndexes().SetMaxTime(10 * time.Second)
+	// create collection index.
+	collection.Indexes().CreateMany(context.Background(), []mongo.IndexModel{
+		{
+			Keys:    bson.M{"userid": 1},
+			Options: options.Index().SetUnique(true),
+		},
+	}, opts)
 	return &userMsgRepository{collection: collection}
 }
 
-func (r *userMsgRepository) InsertOne() {
+func (r *userMsgRepository) InsertOne(msg entity.UserMsg) (*mongo.InsertOneResult, error) {
+	result, err := r.collection.InsertOne(context.Background(), msg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
 
-func (r *userMsgRepository) FetchOne() {
-
+func (r *userMsgRepository) FetchOne(userId string) *mongo.SingleResult {
+	filter := bson.M{
+		"userId": userId,
+	}
+	return r.collection.FindOne(context.Background(), filter)
 }
